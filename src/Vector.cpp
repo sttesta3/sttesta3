161bd1eb63a33948e01e9dc74757cfe5d4249c30
stesta <sttesta@fi.uba.ar>
2023-09-14 21:58:16 -0300

    Primeros metodos

--- src/Vector.cpp
+++ src/Vector.cpp
@@ -1,1 +1,18 @@
 #include "Vector.hpp"
+
+
+Vector::Vector(){
+    this->tamanioMaximo = 0;
+    this->cantidadDatos = 0;
+}
+
+
+bool
+Vector::vacio(){
+    return (this->cantidadDatos == 0);
+}
+
+Vector::~Vector(){
+    while (this->cantidadDatos > 0)
+        this->baja();
+}
