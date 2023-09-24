5edbfb044aa9d594456c865b141b96c89897dd81
stesta <sttesta@fi.uba.ar>
2023-09-24 17:38:32 -0300

    WIP: Debugging Manejo de archivos

--- include/Menu.hpp
+++ include/Menu.hpp
@@ -1,6 +1,9 @@
 #ifndef MENU_H
 #define MENU_H
 
+#include <fstream>
+#include <iostream>
+#include <string.h>
 #include "Vector.hpp"
 
 class Menu {
@@ -10,25 +13,45 @@ class Menu {
         std::string ruta_archivo_salida = "";
         std::string entrada_usuario = "";
 
+        // Función de analisis de entrada del usuario para menu
+        // Pre:
+        // Post: Devuelve un size_t segun la entrada del usuario
         size_t AnalizarEntradaUsuario();
 
         // METODOS DE INTERACCIÓN CON INVENTARIO
+
+        // Pre:
+        // Post: Solicita entrada y carga en inventario
         void Alta();
+        // Pre:
+        // Post: Solicita entrada y elimina primer aparicion en inventario
         void Baja();
+        // Pre:
+        // Post: 
         void Consulta();
 
         // METODOS DE MANEJO DE ARCHIVOS
+
+        // Pre:
+        // Post: Carga archivo en inventario del menu
         void CargarArchivo();
+        // Pre:
+        // Post: Guarda archivo de partida
         void GuardarArchivo();
 
+        // Pre:
+        // Post: Guarda en variable del menu el nombre del archivo. Si no existe devuelve false
         bool SolicitarRutaArchivoEntrada(void);
+        // Pre:
+        // Post: Guarda en variable del menu el nombre del archivo. Si no existe devuelve false
         bool SolicitarRutaArchivoSalida(void);
 
-        
     public:
         Menu();
         ~Menu();
 
+        // Pre:
+        // Post: Corre el juego completo 
         void Juego(void);
 
 };
--- src/Menu.cpp
+++ src/Menu.cpp
@@ -43,12 +43,88 @@ Menu::Juego(void){
 
 bool 
 Menu::SolicitarRutaArchivoEntrada(){
-    return false;
+    bool resultado = false;
+
+    // SOLICITAR ENTRADA DE USUARIO
+    std::cout << "¿Desea cargar desde un savefile?[S/N]: ";
+    std::cin >> this->entrada_usuario;
+    std::cout << std::endl;
+    while (this->entrada_usuario != "S" && this->entrada_usuario != "N"){
+        std::cout << "Entrada invalida, favor reingresar" << std::endl;
+        std::cout << "¿Desea cargar desde un savefile?[S/N]: ";
+        std::cin >> this->entrada_usuario;
+        std::cout << std::endl;
+    }
+
+    // Intentar abrir archivo 
+    if (this->entrada_usuario == "S"){
+        std::cout << "Ingrese ruta a archivo: ";
+        std::cin >> this->entrada_usuario;
+        std::cout << std::endl;
+
+        std::ifstream test;
+        test.open(this->entrada_usuario);
+        if (test.is_open()){
+            test.close();
+            this->ruta_archivo_entrada = this->entrada_usuario;
+            resultado = true;
+        }
+        else
+            std::cout << "Archivo no encontrado" << std::endl;
+    }
+
+    return resultado;
 }
 
 bool 
 Menu::SolicitarRutaArchivoSalida(){
-    return false;
+    std::cout << "¿Desea guardar inventario en savefile?[S/N]: ";
+    std::cin >> this->entrada_usuario;
+    std::cout << std::endl;
+
+    while (this->entrada_usuario != "S" && this->entrada_usuario != "N"){
+        std::cout << "Entrada invalida. Favor reingresar" << std::endl;
+        std::cout << "¿Desea guardar inventario en savefile?[S/N]: ";
+        std::cin >> this->entrada_usuario;
+        std::cout << std::endl;
+    }
+
+    bool guardar = (this->entrada_usuario == "S");
+    if (guardar){
+        if (this->ruta_archivo_entrada != ""){
+            std::cout << "¿Desea sobreescribir archivo de entrada?[S/N]: ";
+            std::cin >> this->entrada_usuario;
+            std::cout << std::endl;
+            while (this->entrada_usuario != "S" && this->entrada_usuario != "N"){
+                std::cout << "Entrada invalida, favor reingresar" << std::endl;
+                std::cout << "¿Desea sobreescribir archivo de entrada?[S/N]: ";
+                std::cin >> this->entrada_usuario;
+                std::cout << std::endl;
+            }
+
+            if (this->entrada_usuario == "N")
+                guardar = false;
+            else
+                this->ruta_archivo_salida = this->ruta_archivo_entrada;
+        }
+        else {
+            std::cout << "Ingrese ruta de archivo de guardado: ";
+            std::cin >> this->entrada_usuario;
+            std::cout << std::endl;
+
+            std::ofstream test;
+            test.open(this->entrada_usuario);
+            if (!test.is_open()){
+                std::cout << "El archivo no fue encontrado y no pudo ser creado." << std::endl;
+                guardar = false;
+            }
+            else
+                test.close();
+
+        }
+    }
+    
+    return guardar;
 }
 
 size_t 
@@ -122,5 +198,34 @@ void Menu::Consulta(){
     std::cout << std::endl;
 }
 
-void Menu::CargarArchivo(){}
-void Menu::GuardarArchivo(){}
+void
+Menu::CargarArchivo(){
+    std::ifstream archivo_entrada;
+    std::string nombre,tipo;
+
+    archivo_entrada.open(this->ruta_archivo_entrada);
+    bool iterar = true;
+    while (iterar){
+        if (getline(archivo_entrada,nombre,',')){
+            getline(archivo_entrada,tipo);
+
+            Item* dato = new Item(nombre,tipo);
+            this->inventario.alta(dato);
+        }
+    }
+    archivo_entrada.close();
+}
+
+
+void Menu::GuardarArchivo(){
+    std::ofstream archivo_salida;
+    std::string linea;
+    archivo_salida.open(this->ruta_archivo_salida);
+
+    while (this->inventario.tamanio() > 0){
+        Item* dato = this->inventario.baja();
+        archivo_salida << dato;
+        delete dato;
+    }
+    archivo_salida.close();
+}
